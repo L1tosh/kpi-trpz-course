a5f223f7f7a565bfd0df8c1cdac46e29b16aa23a
L1tosh <dan.litosh33@gmail.com>
2024-11-25 17:10:13 +0200

    create aspect UserIsOwnerAspect

---
+++ src/main/java/com/software/aspect/UserIsOwnerAspect.java
@@ -0,0 +1,36 @@
+package com.software.aspect;
+
+import com.software.auth.jwt.JwtTokenUtil;
+import com.software.service.ProjectService;
+import com.software.service.impl.ProjectServiceImpl;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class UserIsOwnerAspect {
+
+    private final JwtTokenUtil jwtTokenUtil;
+    private final ProjectService projectService;
+
+    @Autowired
+    public UserIsOwnerAspect(JwtTokenUtil jwtTokenUtil, ProjectServiceImpl projectService) {
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.projectService = projectService;
+    }
+
+    @Before("@annotation(com.software.annotation.CheckUserIsOwner) && args(projectId,..)")
+    public void checkUserInProject(JoinPoint joinPoint, Long projectId) {
+        var userPrincipal = jwtTokenUtil.extractUserPrincipalFromContext();
+
+        var project = projectService.getProjectById(projectId);
+
+        if (!project.getOwner().equals(userPrincipal.getUserId())) {
+            throw new AccessDeniedException("User is not part of the project");
+        }
+    }
+}
